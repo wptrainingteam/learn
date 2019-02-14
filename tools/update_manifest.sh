a176327c5c46a35b968428b363f16349f58b37f6
Jesse C. Owens <jesse@jessecowens.com>
2019-02-14 16:18:06 -0700

    Crude Script to update manifest

--- tools/update_manifest.sh
+++ tools/update_manifest.sh
@@ -4,16 +4,12 @@ printf "[\n" > ../manifest.json
 cd ../lesson-plans/
 
 git submodule foreach -q '
-	readme="https://raw.githubusercontent.com/wptrainingteam/$displaypath/$sha1/README.md";
-	if [ "$(curl -s -w "%{http_code}\n" "$readme" -o /dev/null)" == "200" ];
-	then
 		printf "\t{\n" >> ../../manifest.json;
 		printf "\t\t\"title\": \"$displaypath\",\n" >> ../../manifest.json;
 		printf "\t\t\"slug\": \"$displaypath\",\n" >> ../../manifest.json;
 		printf "\t\t\"markdown_source\": \"https://raw.githubusercontent.com/wptrainingteam/$displaypath/$sha1/README.md\",\n" >> ../../manifest.json;
 		printf "\t\t\"parent\": null\n" >> ../../manifest.json;
 		printf "\t},\n" >> ../../manifest.json;
-	fi
 ';
 
 sed -i '$ d' ../manifest.json
